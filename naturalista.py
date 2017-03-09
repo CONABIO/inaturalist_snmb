b54d68ea74568e9fd9dd2a90cf7c863da7ea2da0
Omar Miranda <mrfumle@users.noreply.github.com>
2017-03-09 10:51:07 -0600

    Conectada a snmb_services
    versión funcional conectada al endpoint snmb_services

--- naturalista.py
+++ naturalista.py
@@ -1,3 +1,5 @@
+# coding=utf-8
+
 import requests
 import urllib, json
 import os
@@ -17,66 +19,24 @@ payload = {
   'username': username,
   'password': password
 }
-#print "POST %s/oauth/token, payload: %s" % (site, payload)
 response = requests.post(("%s/oauth/token" % site), payload)
-#print "RESPONSE"
-#print response.content
-#print
-# response will be a chunk of JSON looking like
-# {
-#   "access_token":"xxx",
-#   "token_type":"bearer",
-#   "expires_in":null,
-#   "refresh_token":null,
-#   "scope":"write"
-# }
 
-# Store the token (access_token) in your app. You can now use it to make authorized
-# requests on behalf of the user, like retrieving profile data:
+
 token = response.json()["access_token"]
-headers_observations = {"Content Type": "application/x-www-form-urlencoded","Authorization": "Bearer %s" % token}
+headers_observations = {"Content Type": "application/x-www-form-urlencoded; charset=utf-8","Authorization": "Bearer %s" % token}
 headers_photo = {"Authorization": "Bearer %s" % token}
-#print "GET %s/users/edit.json, headers: %s" % (site, headers)
-#print "RESPONSE"
-#print requests.get(("%s/users/edit.json" % site), headers=headers).content
-#print requests.get(("%s/observations/5154583.json" % site), headers=headers).content
-#print
 
-############
-#
-# Appending data into Naturalist account
-#
-############
 
 #Getting the information from SNMB database
-#url = "http://coati.conabio.gob.mx/api/v1/naturalista"
-#response = urllib.urlopen(url)
-#snmb_data = json.loads(response.read())
+url = "http://coati.conabio.gob.mx/api/v1/naturalista"
+response = urllib.urlopen(url)
+snmb_data = json.loads(response.read())
 #print snmb_data
 
-payload = [
-       {
-            "observation[species_guess]": "Sylvilagus cunicularius",
-            "observation[observed_on_string]": "2014-08-19T05:00:00.000Z",
-            "observation[latitude]": 19.7361666666667,
-            "observation[longitude]": -103.092666666667,
-            "observation[place_guess]": "Jalisco, Tamazula",
-            "observation[tag_list]": "conglomerado_muestra_id: 256, conglomerado_nombre: 59000, archivo_camara_id: 304, monitoreo_tipo: SAC-MOD",
-            "local_photos[0]": "./132145/2015_09/fotos_videos/Archivo_camara.acb3860a43ead0dc.JPG"
-        },
-        {
-            "observation[species_guess]": "campyylorhynchus brunneicapillus",
-            "observation[observed_on_string]": "2015-11-23T06:00:00.000Z",
-            "observation[latitude]": 23.5254166666667,
-            "observation[longitude]": -110.048166666667,
-            "observation[place_guess]": "Baja California Sur, La Paz, SIERRA LA LAGUNA",
-            "observation[tag_list]": "conglomerado_muestra_id: 1055, conglomerado_nombre: 40429, archivo_camara_id: 191962, monitoreo_tipo: SAR-MOD",
-            "local_photos[0]": "./40429/2015_11/fotos_videos/Archivo_camara.831eb8a44cf4bb80.JPG"
-        }
-]
 
-#for item in snmb_data["data"]:
-for item in payload:
+
+for item in snmb_data["data"]:
+#for item in payload:
     response = requests.post(("%s/observations.json" % site), item, headers=headers_observations)
     print '==================='
     print response.content
